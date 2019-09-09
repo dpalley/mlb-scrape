553775442d09445900fcfd98c3dfa34dd7bee9f1
David Alley <dpalley@gmail.com>
2019-09-09 09:38:50 -0500

    Modified UI, added call to baseballsavant.mlb.com

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from teams import get_teams, get_players
 from stats import get_stats
 import secrets
@@ -12,20 +12,29 @@ app.config['SECRET_KEY'] = secrets.token_hex(24)
 def league():
     teams = get_teams()
     if request.method == 'POST':
-        team = request.form.get('team')
+        team_info = request.form.get('team')
+        # team, dashed_name, division_id = request.form.get('team')
+        team, dashed_name, division_id = team_info.split('.')
         player_names, player_ids = get_players(team)
 
-        return render_template('players.html', team=team,
-            player_info=zip(player_names, player_ids), title='Players')
+        return redirect(url_for('.players', team=team, dashed_name = dashed_name,
+            division_id = division_id, player_names=player_names, player_ids=player_ids, title='Players'))
 
     else:
         return render_template('league.html', teams=teams, title='Teams')
 
 @app.route("/players", methods=['GET', 'POST'])
 def players():
-    player_names, player_ids = get_players(team)
-    return render_template('players.html', team=team,
-        player_info=zip(player_names, player_ids), title='Players')
+    #retrieve data from the return redirect statement in the league() method
+    team = request.args['team']
+    dashed_name = request.args['dashed_name']
+    division_id = request.args['division_id']
+    player_names = request.args.getlist('player_names')
+    player_ids = request.args.getlist('player_ids')
+    title = request.args['title']
+
+    return render_template('players.html', team=team, dashed_name = dashed_name,
+        division_id = division_id, player_info=zip(player_names, player_ids), title=title)
 
 # @app.route("/stats")
 # def player():
--- teams.py
+++ teams.py
@@ -3,36 +3,36 @@ import requests
 import json
 from bs4 import BeautifulSoup as soup
 
-def get_teams():
-    # get html from web
-    page = requests.get('https://www.mlb.com/standings')
-
-    target = soup(page.text, 'lxml')
-
-    pattern = re.compile(r"window.reactHeaderState")
-    my_script = target.find('script', text=pattern)
-    my_script = my_script.text
-
-
-    teams_start = my_script.index('"teamData')
-    teams = my_script[teams_start :]
-
-    # find the matching '}'
-    first_curly = teams.index('{')
+# Isolates a JSON string within a text string.
+# 'blob' is a string of indeterminant length and content, such as a web page.
+# 'start' is the index to begin searching
+# Takes a string of indeterminant length and finds the first '{' character.
+# Finds its matching '}' character and returns a JSON string.
+# Note - this will not work if non-matching '{}' characters are embedded in strings.
+def find_json(blob, start):
+    text = blob[start :]
+    first_curly = text.index('{')
     index = first_curly + 1
     count = 1
     while count > 0:
-        if teams[index] == '{':
+        if text[index] == '{':
             count += 1
-        if teams[index] == '}':
+        if text[index] == '}':
             count -=1
         index += 1
-    # print(f'first curly is {first_curly}, last_curly is {index}')
-    teams = teams[first_curly:index]
+    return text[first_curly:index]
 
-    teams = json.loads(teams)
 
-    return teams
+def get_teams():
+    # get html from web
+    page = requests.get('https://www.mlb.com/standings')
+    target = soup(page.text, 'lxml')
+    pattern = re.compile(r"window.reactHeaderState")
+    my_script = target.find('script', text=pattern)
+    my_script = my_script.text
+    teams = find_json(my_script, my_script.index('"teamData'))
+    return json.loads(teams)
+
 
 def get_players(team):
     page = requests.get('http://m.' + team + '.mlb.com/roster/40-man/')
@@ -42,7 +42,7 @@ def get_players(team):
     player_names = []
     player_ids = []
 
-    for a in roster.find_all('a', class_=''):
+    for a in roster.find_all('a', href=re.compile("player")):
         href = a['href']
         junk1, junk2, player_id, name = href.split('/')
         player_name = name.title().replace('-', ' ')
@@ -50,6 +50,7 @@ def get_players(team):
         player_ids.append(player_id)
 
     return player_names, player_ids
+
 
 def get_teams2():
     page = requests.get('https://www.mlb.com/')
